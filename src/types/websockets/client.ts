71da73d40b4595c0ef851801e3116da2ca360cad
Tiago Siebler @ Siebly.io <tiagosiebler@users.noreply.github.com>
2026-07-17 17:15:52 +0100

    chore(v2.4.5): pin ws to 8.x, remove webpack analyser and improve wsOptions type

--- src/types/websockets/client.ts
+++ src/types/websockets/client.ts
@@ -46,7 +46,10 @@ export interface WSClientConfigurableOptions {
   wsOptions?: {
     protocols?: string[];
     agent?: any;
-  } & Partial<WebSocket.ClientOptions | ClientRequestArgs>;
+  } & (
+    | Omit<Partial<WebSocket.ClientOptions>, 'agent'>
+    | Omit<Partial<ClientRequestArgs>, 'agent'>
+  );
 
   wsUrl?: string;
 
